916b2ac3971c63da9abb5dd0da605f090b82b0c4
SuperScary <wylanshoemaker@gmail.com>
2026-08-05 18:25:55 -0700

    Expand `Tier` enum to support fluid and item transfer properties alongside energy attributes

--- src/main/java/matterfactory/core/Tier.java
+++ src/main/java/matterfactory/core/Tier.java
@@ -6,11 +6,11 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public enum Tier {
 
-	BASIC(8_192, 256),
-	ADVANCED(65_536, 4_096),
-	ELITE(524_288, 32_768),
-	ULTIMATE(4_194_304, 262_144),
-	INFINITE(Integer.MAX_VALUE, Integer.MAX_VALUE);
+	BASIC(8_192, 256, 1024, 1024, 1),
+	ADVANCED(65_536, 4_096, 2048, 2048, 16),
+	ELITE(524_288, 32_768, 4_096, 4_096, 64),
+	ULTIMATE(4_194_304, 262_144, 8_192, 8_192, 128),
+	INFINITE(Integer.MAX_VALUE, Integer.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE, Integer.MAX_VALUE);
 
 	@Getter
 	final BlockBehaviour.Properties blockProperties;
@@ -21,14 +21,26 @@ public enum Tier {
 	@Getter
 	final int energyTransferRate;
 
-	Tier (BlockBehaviour.Properties blockProperties, int energyCapacity, int energyTransferRate) {
+	@Getter
+	final float fluidCapacity; // in mB | 1024 = 1 bucket
+
+	@Getter
+	final float fluidTransferRate; // in mB/t | 1024 = 1 bucket
+
+	@Getter
+	final int itemTransferRate; // in items/t
+
+	Tier (BlockBehaviour.Properties blockProperties, int energyCapacity, int energyTransferRate, float fluidCapacity, float fluidTransferRate, int itemTransferRate) {
 		this.blockProperties = blockProperties;
 		this.energyCapacity = energyCapacity;
 		this.energyTransferRate = energyTransferRate;
+		this.fluidCapacity = fluidCapacity;
+		this.fluidTransferRate = fluidTransferRate;
+		this.itemTransferRate = itemTransferRate;
 	}
 
-	Tier (int energyCapacity, int energyTransferRate) {
-		this(BlockBehaviour.Properties.of().requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.IRON), energyCapacity, energyTransferRate);
+	Tier (int energyCapacity, int energyTransferRate, float fluidCapacity, float fluidTransferRate, int itemTransferRate) {
+		this(BlockBehaviour.Properties.of().requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.IRON), energyCapacity, energyTransferRate, fluidCapacity, fluidTransferRate, itemTransferRate);
 	}
 
 }
